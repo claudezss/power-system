38f414b2f605d06f89b2868040c6e42f28abe640
claudezss <dev.claude.yan.zhang@gmail.com>
2023-06-11 20:50:06 -0400

    Add AssetType enumeration and use it in Model class

---
+++ pyohm/network/assets/model.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, Field
+from typing import Tuple, Optional
+import enum
+
+class AssetType(enum.Enum):
+    """
+    AssetType is an enumeration of the different types of assets that can be
+    represented in a network model.
+    """
+
+    Node = "Node"
+    Line = "Line"
+    Load = "Load"
+    Transformer = "Transformer"
+    Switch = "Switch"
+    Generator = "Generator"
+    Battery = "Battery"
+    PV = "PV"
+    Solar = "Solar"
+    Wind = "Wind"
+
+
+class Model(BaseModel):
+
+    id: str = Field(..., alias="id")
+    name: str = Field(..., alias="name")
+    coordinates: Optional[Tuple[float, float]] = Field(..., alias="coordinates", default=None)
+    asset_type: AssetType = Field(..., alias="asset_type")
+
